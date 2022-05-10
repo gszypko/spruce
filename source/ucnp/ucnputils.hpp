64f46897e440416c7dc62fad63bf3ec9317f6ff9
Grant Gorman <gg25@rice.edu>
2022-05-09 20:37:45 -0500

    update ucnp interfacing

--- source/ucnp/ucnputils.hpp
+++ source/ucnp/ucnputils.hpp
@@ -7,11 +7,13 @@
 #include <filesystem>
 namespace fs = std::filesystem;
 #include "PlasmaSettings.hpp"
+#include "settings.hpp"
 #include "mhd.hpp"
 #include "utils.hpp"
 #include "plasmadomain.hpp"
 
 MhdInp gen_inp_grids_ucnp(const PlasmaSettings& pms);
+MhdInp gen_inp_grids_ucnp(const Settings& pms);
 void genNonUniformGrids(double r_max, int Nr,std::vector<double>& dr,std::string opt);
 
 #endif
