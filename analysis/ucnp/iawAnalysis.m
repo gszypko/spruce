f9db6184834cefff461928f8d564079b7c7ee946
Grant Gorman <gg25@rice.edu>
2022-11-29 15:59:22 -0600

    Update IAW analysis

--- analysis/ucnp/iawAnalysis.m
+++ analysis/ucnp/iawAnalysis.m
@@ -8,6 +8,7 @@ for i = 1:length([data.grids.time])
     s(i).x = data.grids.x_vec;
     [~,indx] = min(abs(s(i).x));
     s(i).n = data.grids.vars(i).n(indx,:);
+    s(i).gam = sqrt(1+s(i).t^2/data.tau^2);
 end
 
 %% Fit 1D Transects with Gaussian
@@ -21,7 +22,10 @@ for i = 1:length([s.t])
     x0(1) = max(ydata); lb(1) = 0; ub(1) = 2*x0(1);
     x0(2) = 0; lb(2) = min(xdata); ub(2) = max(xdata);
     dx = max(xdata) - min(xdata);
-    x0(3) = dx/5; lb(3) = 0; ub(3) = 2*dx;
+    x0(3) = data.sig0; lb(3) = 0; ub(3) = 2*dx;
+    if strcmp(data.settings.n_dist,'uniform')
+        x0(3) = 1000; lb(3) = x0(3)/2; ub(3) = x0(3)*2;
+    end
     x = lsqcurvefit(fun,x0,xdata,ydata,lb,ub);
     s(i).ng = fun(x,xdata);
     s(i).n0 = x(1);
@@ -30,7 +34,7 @@ for i = 1:length([s.t])
     s(i).dntild = s(i).dn./s(i).n0;
 end
 
-% Plot fit results
+% Plot Gaussian fit results
 num = 1;
 [fig,ax,an] = open_subplot(num,'Visible',flags.figvis);
 an.Position = [0.1595    0.9117    0.7230    0.0801];
@@ -77,20 +81,18 @@ for i = 1:length([s.t])
     % c = [A sig k phi]
     xdata = s(i).x;
     ydata = s(i).dntild;
-    fun = @(c,d) c(1).*G(d,1,0,c(2)).*cos(c(3).*d+c(4));
+    fun = @(c,d) G(d,1,0,s(i).sig).*(c(1).*cos(c(2).*d+c(3)));
     dy = max(ydata)-min(ydata);
     x0 = []; lb = []; ub = [];
     x0(1) = dy/2; lb(1) = 0; ub(1) = 5*x0(1);
-    x0(2) = s(i).sig; lb(2) = x0(2)/2; ub(2) = x0(2)*2;
-    L = s(i).sig/2;
-    x0(3) = 2*pi/L; lb(3) = x0(3)/5; ub(3) = x0(3)*5;
-    x0(4) = 0; lb(4) = 0; ub(4) = 2*pi;
+    L = data.settings.n_iaw_sig*s(i).gam;
+    x0(2) = 2*pi/L; lb(2) = x0(2)/5; ub(2) = x0(2)*5;
+    x0(3) = pi/2; lb(3) = 0; ub(3) = 2*pi;
     x = lsqcurvefit(fun,x0,xdata,ydata,lb,ub);
     s(i).dntild_fit = fun(x,xdata);
     s(i).dntild_guess = fun(x0,xdata);
     s(i).A = x(1);
-    s(i).sig2 = x(2);
-    s(i).k = x(3);
+    s(i).k = x(2);
     s(i).lam = 2*pi/s(i).k;
 end
 
@@ -135,6 +137,44 @@ close(fig)
 filepath = [data.folder filesep 'iaw-dntild'];
 write_video(filepath,frames);
 
-%% Plot Fitted Wavelength and Plasma Size
+%% Plot Lengthscale Evolution - Fitted Wavelength and Plasma Size
+
+% Plot size evolution
+num = 1;
+[fig,ax,an] = open_subplot(num,'Visible',flags.figvis);
+an.Position = [0.1595    0.9117    0.7230    0.0801];
+cax = get_axis(fig,ax{1});
+q = {'sig','lam','gam','A'};
+qstr = {'$\sigma / \sigma_0$','$\lambda / \lambda_0$','$\sqrt{1+t^2/\tau^2}$','$A / A_0$'};
+lgdstr = qstr;
+l = get_line_specs(length(lgdstr));
+hold off
+for k = 1:length(q)
+    xdata = [s.t];
+    ydata = [s.(q{k})];
+    ydata = ydata./ydata(1);
+    lp = l(k);
+    plot(xdata,ydata,lp.style,'LineWidth',2,'MarkerSize',4,'Color',lp.col,'MarkerFaceColor',lp.col,'MarkerEdgeColor',lp.col)
+    hold on
+end
+cax.PlotBoxAspectRatio = [1 1 1];
+cax.FontSize = 12;
+grid on
+grid minor
+
+xlabel('x (cm)')
+ylabel('Length Scale Evolution','Interpreter','latex')
+
+lgd = legend(lgdstr,'Interpreter','latex');
+lgd.Position = [0.1821    0.7597    0.2681    0.1444];
+
+dlm = ' - ';
+str1 = ['Iter = ' num2str(i-1)];
+str2 = ['t = ' num2str(s(i).t*1e6,'%.3g') '\mus = ' num2str(s(i).t/data.tau,'%.3g') '\tau_e_x_p'];
+an.String = [str1 dlm str2];
+
+filepath = [data.folder filesep 'iaw-length-evol.png'];
+saveas(fig,filepath);
+close(fig)
 
 end
--- analysis/ucnp/main.m
+++ analysis/ucnp/main.m
@@ -2,23 +2,21 @@
 clc, clearvars -except data, close all, f = filesep; setpath;
 
 inp = {'folder';
-    'C:\Users\grant\OneDrive\Research\mhd\data-sims\11.17.22\set_0';
-    'C:\Users\grant\OneDrive\Research\mhd\data-sims\11.17.22\set_1';
-
+    'C:\Users\Grant\OneDrive\Research\mhd\data-sims\11.15.22\set_3';
       };
 s = spreadsheet2struct(inp,inp(1,:));
 Te = [];
 
 % simulation flags
-flags.sim.plot_grids = true;
+flags.sim.plot_grids = false;
 flags.sim.vlasov_analysis = false;
 flags.sim.cmpr_exp_sim = false;
 flags.sim.ion_holes = false;
 flags.sim.hole_orientation = 0;
-flags.sim.iaw_analysis = false;
+flags.sim.iaw_analysis = true;
 
-% flags.sim.vars = {'n', 'v_x', 'v_y', 'temp', 'dt'};
-flags.sim.vars = {'i_n', 'dn', 'i_v_x', 'j_x', 'i_temp', 'e_temp', 'E_x', 'dt'};
+flags.sim.vars = {'n', 'v_x', 'v_y', 'temp', 'dt'};
+% flags.sim.vars = {'i_n', 'dn', 'i_v_x', 'j_x', 'i_temp', 'e_temp', 'E_x', 'dt'};
 flags.sim.plot_freq = 1;
 flags.sim.ghost_cells = 2;
 flags.sim.eic_opt = true;
@@ -26,17 +24,17 @@ flags.sim.figvis = 'on';
 
 % experimental flags
 flags.exp.gen_state = true; % turns on the script to generate init.state from experimental conditions
-flags.exp.set = 15; % unique set identifier - modifies directory structure as \...\set_XXX
-flags.exp.num_time_pts = 1000; % sets time interval for recording grids during simulation
+flags.exp.set = 16; % unique set identifier - modifies directory structure as \...\set_XXX
+flags.exp.num_time_pts = 200; % sets time interval for recording grids during simulation
 flags.exp.smooth_density = true; % whether or not to apply an SG filter to the density distribution
-flags.exp.sg_imgs_length = 0.01; % length scale (cm) for SG kernel when filtering density distribution
+flags.exp.sg_imgs_length = 0.02; % length scale (cm) for SG kernel when filtering density distribution
 flags.exp.sim_window = [.5 .5]; % [x y] domain limits in cm, symmetric about zero
 flags.exp.exp_window = [.55 .55]; % [x y] experimental domain limits in cm, symmetric about zero
 flags.exp.sg_back_length = .05;
-flags.exp.Nx = 201; % number of points on x axis for simulation domain
-flags.exp.Ny = 201; % number of points on y axis for simulation domain
+flags.exp.Nx = 301; % number of points on x axis for simulation domain
+flags.exp.Ny = 301; % number of points on y axis for simulation domain
 flags.exp.n_min = 1e5; % minimum in simulation density distribution cm^-^3
-flags.exp.Te = 200;
+flags.exp.Te = 100;
 flags.exp.useLIFFits = false;
 flags.exp.figvis = 'on';
 flags.exp.config_path = 'C:\Users\grant\Documents\GitHub\mhd\ucnp.config';
