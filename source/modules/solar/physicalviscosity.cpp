664c5d3e4fd53e7115d8939ee9b8bab460a4c50d
gszypko <gszypko@gmail.com>
2025-03-27 16:09:01 -0500

    modified physical viscosity module to account for gradient correction with falloff

--- source/modules/solar/physicalviscosity.cpp
+++ source/modules/solar/physicalviscosity.cpp
@@ -101,19 +101,21 @@ std::vector<Grid> PhysicalViscosity::computeViscousForce(const std::vector<Grid>
         for(int i=0; i<2; i++){
             if(i == j) delta = 1.0/3.0;
             else delta = 0.0;
+            //NOTE: Calculation is split up into three terms to expose the direct second-order velocity gradients (in grad_b_terms_diag)
             result[j] += m_pd.derivative1D(
-                            temperature.pow(2.5)
+                            temperature.pow(2.5)*coeff_grid
                             *(delta - b_hat[i]*b_hat[j])
                             *same_factor_off_diag, i);
             // result[j] += grad(b terms) dot (tensor)
             // result[j] += (b terms) div (tensor)
-            result[j] += grad_b_terms_diag[i]*temperature.pow(2.5)
+            result[j] += grad_b_terms_diag[i]*temperature.pow(2.5)*coeff_grid
                             *(delta - b_hat[i]*b_hat[j]);
             result[j] += same_factor_diag * m_pd.derivative1D(
-                            temperature.pow(2.5)
+                            temperature.pow(2.5)*coeff_grid
                             *(delta - b_hat[i]*b_hat[j]), i);
         }
-        result[j] *= -3.0*coeff_grid*m_pd.m_ghost_zone_mask;
+        // result[j] *= -3.0*coeff_grid*m_pd.m_ghost_zone_mask;
+        result[j] *= -3.0*m_pd.m_ghost_zone_mask;
     }
     return result;
 }
