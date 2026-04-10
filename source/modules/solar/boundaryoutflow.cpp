606692ff8a81b5806e1ce1b9f39cf97e03d05722
gszypko <gszypko@gmail.com>
2026-04-09 23:30:17 -0500

    implemented dynamic boundary outflow driving

--- source/modules/solar/boundaryoutflow.cpp
+++ source/modules/solar/boundaryoutflow.cpp
@@ -23,34 +23,44 @@ void BoundaryOutflow::parseModuleConfigs(std::vector<std::string> lhs, std::vect
         else if(this_lhs == "falloff_shape") falloff_shape = this_rhs;
         else if(this_lhs == "feather_length") feather_length = std::stod(this_rhs);
         else if(this_lhs == "field_aligned_mode") field_aligned_mode = (this_rhs == "true");        
+        else if(this_lhs == "dynamic_mode") dynamic_mode = (this_rhs == "true");
+        else if(this_lhs == "dynamic_time") dynamic_time = std::stod(this_rhs);
+        else if(this_lhs == "dynamic_target_speed") dynamic_target_speed = std::stod(this_rhs);
         else std::cerr << this_lhs << " config not recognized.\n";
     }
 }
 
 void BoundaryOutflow::setupModule(){
     assert((falloff_shape == "exp" || falloff_shape == "gaussian") && "BoundaryOutflow shape must be exp or gaussian");
-    accel_template = constructBoundaryAccel(max_accel,falloff_length);
+    accel_template = constructBoundaryAccel(falloff_length,feather_length);
+    mean_outflow = computeMeanOutflow();
 }
 
 void BoundaryOutflow::postIterateModule(double dt){
+    curr_accel = max_accel;
+    if(dynamic_mode){
+        mean_outflow = computeMeanOutflow();
+        curr_accel = (dynamic_target_speed - mean_outflow)/dynamic_time;
+        curr_accel = std::min(curr_accel,max_accel);
+        curr_accel = std::max(curr_accel,0.0);
+    }
+    Grid accel = curr_accel*accel_template;
     if (boundary=="x_bound_1" || boundary=="x_bound_2"){
         if(field_aligned_mode){
-            Grid dot = m_pd.m_eqs->grid(IdealMHD::b_hat_x)*accel_template;
-            m_pd.m_eqs->grid(IdealMHD::mom_x) += (dt*dot*m_pd.m_eqs->grid(IdealMHD::b_hat_x)*m_pd.m_eqs->grid(IdealMHD::rho));
-            m_pd.m_eqs->grid(IdealMHD::mom_y) += (dt*dot*m_pd.m_eqs->grid(IdealMHD::b_hat_y)*m_pd.m_eqs->grid(IdealMHD::rho));
+            m_pd.m_eqs->grid(IdealMHD::mom_x) += (dt*accel*m_pd.m_eqs->grid(IdealMHD::b_hat_x)*m_pd.m_eqs->grid(IdealMHD::rho));
+            m_pd.m_eqs->grid(IdealMHD::mom_y) += (dt*accel*m_pd.m_eqs->grid(IdealMHD::b_hat_y)*m_pd.m_eqs->grid(IdealMHD::rho));
         }
         else{
-            m_pd.m_eqs->grid(IdealMHD::mom_x) += (dt*accel_template*m_pd.m_eqs->grid(IdealMHD::rho));
+            m_pd.m_eqs->grid(IdealMHD::mom_x) += (dt*accel*m_pd.m_eqs->grid(IdealMHD::rho));
         }
     }
     else if (boundary=="y_bound_1" || boundary=="y_bound_2"){
         if(field_aligned_mode){
-            Grid dot = m_pd.m_eqs->grid(IdealMHD::b_hat_y)*accel_template;
-            m_pd.m_eqs->grid(IdealMHD::mom_x) += (dt*dot*m_pd.m_eqs->grid(IdealMHD::b_hat_x)*m_pd.m_eqs->grid(IdealMHD::rho));
-            m_pd.m_eqs->grid(IdealMHD::mom_y) += (dt*dot*m_pd.m_eqs->grid(IdealMHD::b_hat_y)*m_pd.m_eqs->grid(IdealMHD::rho));
+            m_pd.m_eqs->grid(IdealMHD::mom_x) += (dt*accel*m_pd.m_eqs->grid(IdealMHD::b_hat_x)*m_pd.m_eqs->grid(IdealMHD::rho));
+            m_pd.m_eqs->grid(IdealMHD::mom_y) += (dt*accel*m_pd.m_eqs->grid(IdealMHD::b_hat_y)*m_pd.m_eqs->grid(IdealMHD::rho));
         }
         else{
-            m_pd.m_eqs->grid(IdealMHD::mom_y) += (dt*accel_template*m_pd.m_eqs->grid(IdealMHD::rho));
+            m_pd.m_eqs->grid(IdealMHD::mom_y) += (dt*accel*m_pd.m_eqs->grid(IdealMHD::rho));
         }
     }
     m_pd.m_eqs->propagateChanges();
@@ -58,10 +68,12 @@ void BoundaryOutflow::postIterateModule(double dt){
 
 std::string BoundaryOutflow::commandLineMessage() const
 {
-    return boundary + " boundary outflow enforced";
+    std::string result = boundary + " boundary outflow enforced (mean " + std::to_string(mean_outflow) + " cm/s outflow)";
+    if(dynamic_mode) result = result + " (dynamic accel. " + std::to_string(curr_accel) +" cm/s^2)";
+    return result;
 }
 
-Grid BoundaryOutflow::constructBoundaryAccel(double strength,double length) const
+Grid BoundaryOutflow::constructBoundaryAccel(double length, double feather) const
 {
     const Grid& x = m_pd.grid("pos_x");
     const Grid& y = m_pd.grid("pos_y");
@@ -81,26 +93,126 @@ Grid BoundaryOutflow::constructBoundaryAccel(double strength,double length) cons
 
     Grid result;
     if(falloff_shape == "exp"){
-        if (boundary=="x_bound_1") result = (-2.3*(x - x.min())/length).exp()*strength;
-        else if (boundary=="x_bound_2") result = (2.3*(x - x.max())/length).exp()*strength;
-        else if (boundary=="y_bound_2") result = (2.3*(y - y.max())/length).exp()*strength;
-        else if (boundary=="y_bound_1") result = (-2.3*(y - y.min())/length).exp()*strength;
+        if (boundary=="x_bound_1") result = (-2.3*(x - x.min())/length).exp();
+        else if (boundary=="x_bound_2") result = (2.3*(x - x.max())/length).exp();
+        else if (boundary=="y_bound_2") result = (2.3*(y - y.max())/length).exp();
+        else if (boundary=="y_bound_1") result = (-2.3*(y - y.min())/length).exp();
     } else if(falloff_shape == "gaussian"){
-        if (boundary=="x_bound_1") result = (-2.3*((x - x.min())/length).square()).exp()*strength;
-        else if (boundary=="x_bound_2") result = (-2.3*((-x + x.max())/length).square()).exp()*strength;
-        else if (boundary=="y_bound_2") result = (-2.3*((-y + y.max())/length).square()).exp()*strength;
-        else if (boundary=="y_bound_1") result = (-2.3*((y - y.min())/length).square()).exp()*strength;
+        if (boundary=="x_bound_1") result = (-2.3*((x - x.min())/length).square()).exp();
+        else if (boundary=="x_bound_2") result = (-2.3*((-x + x.max())/length).square()).exp();
+        else if (boundary=="y_bound_2") result = (-2.3*((-y + y.max())/length).square()).exp();
+        else if (boundary=="y_bound_1") result = (-2.3*((y - y.min())/length).square()).exp();
     } else {
         assert(false && "Unexpected option for BoundaryOutflow falloff_shape");
     }
 
-    if(feather_length > 0.0){
-        if (boundary=="x_bound_1" || boundary=="x_bound_2") result *= ((-2.3*((y - (y.max()-2.0*feather_length)).max(0.0)/feather_length).square()).exp()
-                                                                        *(-2.3*((y - (y.min()+2.0*feather_length)).min(0.0)/feather_length).square()).exp()
+    if(feather > 0.0){
+        if (boundary=="x_bound_1" || boundary=="x_bound_2") result *= ((-2.3*((y - (y.max()-2.0*feather)).max(0.0)/feather).square()).exp()
+                                                                        *(-2.3*((y - (y.min()+2.0*feather)).min(0.0)/feather).square()).exp()
                                                                           -0.01).max(0.0);
-        else if (boundary=="y_bound_1" || boundary=="y_bound_2") result *= ((-2.3*((x - (x.max()-2.0*feather_length)).max(0.0)/feather_length).square()).exp()
-                                                                            *(-2.3*((x - (x.min()+2.0*feather_length)).min(0.0)/feather_length).square()).exp()
+        else if (boundary=="y_bound_1" || boundary=="y_bound_2") result *= ((-2.3*((x - (x.max()-2.0*feather)).max(0.0)/feather).square()).exp()
+                                                                            *(-2.3*((x - (x.min()+2.0*feather)).min(0.0)/feather).square()).exp()
                                                                              -0.01).max(0.0);
     }
     return ghost_mask_withboundary*result;
 }
+
+double BoundaryOutflow::computeMeanOutflow() const
+{
+    const Grid& x = m_pd.grid("pos_x");
+    const Grid& y = m_pd.grid("pos_y");
+
+    int xl = m_pd.m_xl, xu = m_pd.m_xu, yl = m_pd.m_yl, yu = m_pd.m_yu;
+    if (boundary=="x_bound_1") xl = m_pd.m_xl_dt;
+    else if (boundary=="x_bound_2") xu = m_pd.m_xu_dt;
+    else if (boundary=="y_bound_2") yu = m_pd.m_yu_dt;
+    else if (boundary=="y_bound_1") yl = m_pd.m_yl_dt;
+
+    if(boundary=="x_bound_1" || boundary=="x_bound_2"){
+        //apply feathering offsets
+        for(int j = yl; j <= yu; j++){
+            if(y(0,j) - y(0,yl) >= feather_length){
+                yl = j;
+                break;
+            }
+        }
+        for(int j = yu; j >= yl; j--){
+            if(y(0,yu) - y(0,j) >= feather_length){
+                yu = j;
+                break;
+            }
+        }
+        //apply falloff offset
+        if(boundary=="x_bound_1"){
+            for(int i = xl; i <= xu; i++){
+                if(x(i,0) - x(xl,0) >= falloff_length){
+                    xu = i;
+                    break;
+                }
+            }
+        }
+        else{ //(boundary=="x_bound_2")
+            for(int i = xu; i >= xl; i--){
+                if(x(xu,0) - x(i,0) >= falloff_length){
+                    xl = i;
+                    break;
+                }
+            }
+        }
+    }
+    else{
+        assert(boundary=="y_bound_1" || boundary=="y_bound_2" && "Unexpected boundary specified");
+        //apply feathering offset
+        for(int i = xl; i <= xu; i++){
+            if(x(i,0) - x(xl,0) >= feather_length){
+                xl = i;
+                break;
+            }
+        }
+        for(int i = xu; i >= xl; i--){
+            if(x(xu,0) - x(i,0) >= feather_length){
+                xu = i;
+                break;
+            }
+        }
+        //apply falloff offset
+        if(boundary=="y_bound_1"){
+            for(int j = yl; j <= yu; j++){
+                if(y(0,j) - y(0,yl) >= falloff_length){
+                    yu = j;
+                    break;
+                }
+            }
+        }
+        else{ //(boundary=="y_bound_2")
+            for(int j = yu; j >= yl; j--){
+                if(y(0,yu) - y(0,j) >= falloff_length){
+                    yl = j;
+                    break;
+                }
+            }
+        }
+    }
+    
+    const Grid& b_hat_x = m_pd.m_eqs->grid(IdealMHD::b_hat_x);
+    const Grid& b_hat_y = m_pd.m_eqs->grid(IdealMHD::b_hat_y);
+    const Grid& v_x = m_pd.m_eqs->grid(IdealMHD::v_x);
+    const Grid& v_y = m_pd.m_eqs->grid(IdealMHD::v_y);
+
+    int cells = 0;
+    double sum = 0.0;
+    for(int i = xl; i <= xu; i++){
+        for(int j = yl; j <= yu; j++){
+            cells++;
+            double summand = b_hat_x(i,j)*v_x(i,j) + b_hat_y(i,j)*v_y(i,j);
+            //correct for orientation of field to ensure we are targeting outflow
+            if (boundary=="x_bound_1" && b_hat_x(i,j) > 0.0) summand *= -1.0;
+            else if (boundary=="x_bound_2" && b_hat_x(i,j) < 0.0) summand *= -1.0;
+            else if (boundary=="y_bound_2" && b_hat_y(i,j) < 0.0) summand *= -1.0;
+            else if (boundary=="y_bound_1" && b_hat_y(i,j) > 0.0) summand *= -1.0;
+            sum += summand;
+        }
+    }
+    return sum / (double)(cells);
+}
+
--- source/modules/solar/boundaryoutflow.hpp
+++ source/modules/solar/boundaryoutflow.hpp
@@ -17,12 +17,18 @@ class BoundaryOutflow : public Module {
         double max_accel; //maximum acceleration to apply, reached at center of gaussian injection template
         double falloff_length; //scale height of exponential falloff from boundary
         double feather_length{0.0}; //scale length to smooth the edges of the outflow acceleration to zero; ramps over about two times this length
+        double mean_outflow{0.0};
+        double dynamic_time{1.0};
+        double dynamic_target_speed{0.0};
+        double curr_accel{0.0};
         Grid accel_template;
         std::string boundary{"y_bound_2"};
         std::string falloff_shape{"exp"};
         bool field_aligned_mode{false};
+        bool dynamic_mode{false};
         void parseModuleConfigs(std::vector<std::string> lhs, std::vector<std::string> rhs) override;
         Grid constructBoundaryAccel(double strength,double length) const;
+        double computeMeanOutflow() const;
 };
 
 #endif
